823d4853dd0d31789c70772fecc7683193dadba0
Manikanta.Mareedu <23p31a4422@acet.ac.in>
2026-02-18 21:19:07 +0530

    Time: 0 ms (100%), Space: 8 MB (10.25%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        string s = "";
+        while(n > 0){
+            s += n%2;
+            n = n/2;
+            
+        }
+        for(int i = 0; i < s.size() - 1; i++){
+            if(s[i] == s[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
